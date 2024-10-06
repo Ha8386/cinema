c15c269fc61b04ff1d1e299be02377cd12fad3ae
Ha8386 <haxyz04@gmail.com>
2024-10-06 12:06:09 +0700

    database

---
+++ database/create_database.sql
@@ -0,0 +1,303 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 06, 2024 lúc 06:44 AM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `quanly_4scinema`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `cinemas`
+--
+
+CREATE TABLE `cinemas` (
+  `cinema_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `capacity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL,
+  `customer_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+--
+-- Cấu trúc bảng cho bảng `employees`
+--
+
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL,
+  `employee_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `position` varchar(100) DEFAULT NULL,
+  `hire_date` date DEFAULT NULL,
+  `salary` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `movies`
+--
+
+CREATE TABLE `movies` (
+  `movie_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `genre` varchar(100) NOT NULL,
+  `age_rating` varchar(10) NOT NULL,
+  `release_date` date NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `subtitle` varchar(50) DEFAULT NULL,
+  `status` varchar(50) NOT NULL,
+  `actors` text DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `trailer_url` varchar(255) DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `promotion`
+--
+
+CREATE TABLE `promotion` (
+  `id` int(11) NOT NULL,
+  `promotion_name` varchar(255) NOT NULL,
+  `details` text DEFAULT NULL,
+  `notes` text DEFAULT NULL,
+  `conditions` text DEFAULT NULL,
+  `discount_image` varchar(255) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `revenue_summary`
+--
+
+CREATE TABLE `revenue_summary` (
+  `id` int(11) NOT NULL,
+  `total_amount` decimal(10,2) NOT NULL,
+  `transaction_count` int(11) NOT NULL,
+  `report_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `screenings`
+--
+
+CREATE TABLE `screenings` (
+  `screening_id` int(11) NOT NULL,
+  `showtime_id` int(11) DEFAULT NULL,
+  `screening_time` time DEFAULT NULL,
+  `status` enum('available','sold out','coming soon') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `showtimes`
+--
+
+CREATE TABLE `showtimes` (
+  `showtime_id` int(11) NOT NULL,
+  `movie_id` int(11) DEFAULT NULL,
+  `cinema_id` int(11) DEFAULT NULL,
+  `show_date` date DEFAULT NULL,
+  `show_time` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `ticket_orders`
+--
+
+CREATE TABLE `ticket_orders` (
+  `id` int(11) NOT NULL,
+  `movie_name` varchar(255) NOT NULL,
+  `customer_name` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `order_date` datetime DEFAULT current_timestamp(),
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `cinemas`
+--
+ALTER TABLE `cinemas`
+  ADD PRIMARY KEY (`cinema_id`);
+
+--
+-- Chỉ mục cho bảng `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Chỉ mục cho bảng `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Chỉ mục cho bảng `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`movie_id`);
+
+--
+-- Chỉ mục cho bảng `promotion`
+--
+ALTER TABLE `promotion`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `revenue_summary`
+--
+ALTER TABLE `revenue_summary`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `screenings`
+--
+ALTER TABLE `screenings`
+  ADD PRIMARY KEY (`screening_id`),
+  ADD KEY `showtime_id` (`showtime_id`);
+
+--
+-- Chỉ mục cho bảng `showtimes`
+--
+ALTER TABLE `showtimes`
+  ADD PRIMARY KEY (`showtime_id`),
+  ADD KEY `movie_id` (`movie_id`),
+  ADD KEY `cinema_id` (`cinema_id`);
+
+--
+-- Chỉ mục cho bảng `ticket_orders`
+--
+ALTER TABLE `ticket_orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `cinemas`
+--
+ALTER TABLE `cinemas`
+  MODIFY `cinema_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT cho bảng `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `movie_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `promotion`
+--
+ALTER TABLE `promotion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `revenue_summary`
+--
+ALTER TABLE `revenue_summary`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `screenings`
+--
+ALTER TABLE `screenings`
+  MODIFY `screening_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `showtimes`
+--
+ALTER TABLE `showtimes`
+  MODIFY `showtime_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `ticket_orders`
+--
+ALTER TABLE `ticket_orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `screenings`
+--
+ALTER TABLE `screenings`
+  ADD CONSTRAINT `screenings_ibfk_1` FOREIGN KEY (`showtime_id`) REFERENCES `showtimes` (`showtime_id`);
+
+--
+-- Các ràng buộc cho bảng `showtimes`
+--
+ALTER TABLE `showtimes`
+  ADD CONSTRAINT `showtimes_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`),
+  ADD CONSTRAINT `showtimes_ibfk_2` FOREIGN KEY (`cinema_id`) REFERENCES `cinemas` (`cinema_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
