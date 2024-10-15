ee15531497890643f7af3ce8f67f01c84b90bacb
Ha8386 <haxyz04@gmail.com>
2024-10-15 15:13:23 +0700

    xoá chuyển trang

--- admin/movie_management/ad_movie.php
+++ admin/movie_management/ad_movie.php
@@ -349,13 +349,7 @@ if (isset($_POST['updatemovie'])) {
                 </tbody>
 
             </table>
-            <div class="pagination">
-                <a href="#" class="active">1</a>
-                <a href="#">2</a>
-                <a href="#">3</a>
-                <a href="#">4</a>
-                <a href="#">5</a>
-            </div>
+            
         </div>
     </div>
            
